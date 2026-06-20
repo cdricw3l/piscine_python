519d4c2452381855aec8a8e93aa659e575bbdef3
cdric.B <cdric.b@cdric.local>
2026-06-20 21:21:34 +0200

    python_03/ex0

--- p03/ex6/ft_data_alchemist.py
+++ p03/ex6/ft_data_alchemist.py
@@ -1,3 +1,5 @@
+import random
+
 if __name__ == "__main__":
     print(f"=== Game Data Alchemist ===\n")
     player_list: list[str] =  ['Alice', 'bob', 'Charlie', 'dylan', 'Emma', 'Gregory', 'john', 'kevin', 'Liam']
@@ -6,3 +8,9 @@ if __name__ == "__main__":
     print(f"New list with all names capitalized: {capitalize_list}")
     capitalized_only_list : list[str] = [player for player in player_list if player == player.capitalize()]
     print(f"New list of capitalized names only: {capitalized_only_list}")
+    score_list: dict[str, int] = {player: random.randrange(1,500) for player in player_list}
+    print(f"Score dict: {score_list}")
+    score_avg: int = round(sum([ score_list[v] for v in score_list]) / len(score_list), 2)
+    print(f"Score average is {score_avg}")
+    high_score: dict[str, int] = {k:score_list[k] for k in score_list if score_list[k] > score_avg}
+    print(f"High scores: {high_score}")
