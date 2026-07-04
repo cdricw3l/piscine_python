509ebe42535777f5dfb8538be42542c65c0b1f66
cdric.B <cdric.b@cdric.local>
2026-07-04 19:15:58 +0200

    ex

--- p07/ex0/main.py
+++ p07/ex0/main.py
@@ -5,8 +5,8 @@ class Creature(ABC):
     __creature_type: str
 
     def __init__(self, creature_name: str, creature_type: str):
-        self.___creature_name = creature_name
-        self.__creature_type = creature_type
+        self.___creature_name = creature_name.capitalize()
+        self.__creature_type = creature_type.capitalize()
 
     def get_name(self) -> str:
         return self.___creature_name
@@ -18,9 +18,36 @@ class Creature(ABC):
     def attack(self, attack: str) -> str:
         pass
 
+    def describe(self) -> str:
+        return f"{self.___creature_name} is a {self.__creature_type} type Creature"
+
 class Flameling(Creature):
 
-    def __init__(self, creature_name: str, creature_type: str):
+    def __init__(self, creature_name: str, creature_type: str = 'Fire'):
+        super().__init__(creature_name, creature_type)
+    
+    def attack(self, attack: str) -> str:
+        return f"{self.___creature_name} use {attack}"
+
+class Pyrodon(Creature):
+
+    def __init__(self, creature_name: str, creature_type: str = ' Fire/Flying'):
+        super().__init__(creature_name, creature_type)
+    
+    def attack(self, attack: str) -> str:
+        return f"{self.___creature_name} use {attack}"
+
+class Aquabub(Creature):
+
+    def __init__(self, creature_name: str, creature_type: str = 'Water'):
+        super().__init__(creature_name, creature_type)
+    
+    def attack(self, attack: str) -> str:
+        return f"{self.___creature_name} use {attack}"
+
+class Torragon(Creature):
+
+    def __init__(self, creature_name: str, creature_type: str = 'Water'):
         super().__init__(creature_name, creature_type)
     
     def attack(self, attack: str) -> str:
