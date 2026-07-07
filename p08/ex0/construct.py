98d9625427f87ec48deeb2953ea2b8bd5aecba3a
Cedric Bouhadra <cebouhad@c4b5c6.42berlin.de>
2026-07-07 18:20:25 +0200

    p08

---
+++ p08/ex0/construct.py
@@ -0,0 +1,46 @@
+import sys
+import site
+
+
+def interpreter() -> str | None:
+    path: str = sys.executable
+    python: str = path.split('/')[len(path.split('/')) - 1]
+    if python == 'python':
+        return f"{sys.executable}{sys.version_info[0]}.{sys.version_info[1]}"
+    elif python == "python3":
+        return f"{sys.executable}.{sys.version_info[1]}"
+    return None
+
+
+def get_vitual_env() -> str:
+    path: str = sys.prefix
+    venv: str = path.split("/")[len(path.split("/")) - 1]
+    return venv
+
+
+def instruction() -> None:
+    print("To enter the construct, run:")
+    print("python -m venv matrix_env")
+    print("source matrix_env/bin/activate # On Unix")
+    print("matrix_env\\Scripts\\activate # On Windows\n")
+
+
+if __name__ == "__main__":
+    if sys.base_prefix == sys.prefix:
+        print("\nMATRIX STATUS: You're still plugged in\n")
+        print(f"Current Python: {interpreter()}")
+        print("Virtual Environment: None detected\n")
+        print("WARNING: You're in the global environment")
+        print("The machines can see everything you install.\n")
+        instruction()
+        print("Then run this program again")
+    else:
+        print("\nMATRIX STATUS: Welcome to the construct\n")
+        print(f"Current Python: {sys.executable}")
+        print(f"Virtual Environment: {get_vitual_env()}")
+        print(f"Environment Path: {sys.prefix}\n")
+        print("SUCCESS: You're in an isolated environment!")
+        print("Safe to install packages without affecting")
+        print("the global system.\n")
+        print("Package installation path:")
+        print(f"{site.getsitepackages()[0]}")
---
+++ p08/ex1/loading.py
@@ -0,0 +1,107 @@
+from importlib import util, metadata
+import sys
+
+class Color_line():
+    BLACK: str = "\033[90m"
+    RED: str = "\033[91m"
+    GREEN: str = "\033[92m"
+    YELLOW: str = "\033[93m"
+    BLUE: str = "\033[94m"
+    PURPLE: str = "\033[95m"
+    CYAN: str = "\033[96m"
+    WHITE: str = "\033[97m"
+    RESET: str = "\033[0m"
+
+def venv_instruction() -> None:
+    print(f"{Color_line.YELLOW}python -m venv \x1B[3m<environement-name>\x1B[0m")
+    print(f"{Color_line.YELLOW}source matrix_env/bin/activate # On Unix")
+    print(f"matrix_env\\Scripts\\activate # On Windows{Color_line.RESET}\n")
+
+
+
+def get_plot_color(key: str) -> str:
+    match key:
+        case '< 100':
+            return 'bo'
+        case '101/200':
+            return 'ro'
+        case '201/300':
+            return 'go'
+        case '301/400':
+            return 'yo'
+        case '401/500':
+            return 'co'
+   
+
+def get_pie_color(key: str) -> str:
+    match key:
+        case '< 100':
+            return 'blue'
+        case '101/200':
+            return 'red'
+        case '201/300':
+            return 'green'
+        case '301/400':
+            return 'yellow'
+        case '401/500':
+            return 'cyan'
+   
+
+def create_data_set():
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    arr: np.typing.NDArray = np.random.randint(low=0, high=500, size=(1,1000)).round(2)
+    frame = pd.DataFrame({"value": arr[0]})
+    sets: list[pd.DataFrame] = {
+        '< 100':frame.query('value <= 100'),
+        '101/200':frame.query('value > 100 and value <= 200'),
+        '201/300':frame.query('value > 200 and value <= 300'),
+        '301/400':frame.query('value > 300 and value <= 400'),
+        '401/500':frame.query('value > 400 and value <= 500'),
+    }
+    fig, ax = plt.subplots(2)
+    pie: list[int] = [len(list(sets[x]['value'])) for x in sets]
+    label: list[str] = [x for x in sets]
+    color: list[str] = [get_pie_color(x) for x in sets]
+
+    ax[1].set_xlabel('distribution index')
+    ax[1].set_ylabel('distribution value')
+    for x in sets:
+        ax[0].pie(pie,labels= label, colors=color, autopct='%1.1f%%')
+        ax[1].plot(sets[x], get_plot_color(x))
+        ax[1].plot(sets[x], get_plot_color(x))
+    try:
+        plt.savefig("matrix_analysis.png")
+    except Exception as e:
+        print(e)
+
+def check_dependancy(dependencys: list[str]) -> None:
+    is_complete: bool =  True
+    for dependency in dependencys:
+        dependency = dependency.strip()
+        if util.find_spec(dependency) is not None:
+            version = metadata.version(dependency)
+            print(f"{Color_line.GREEN}[OK] {dependency}{Color_line.RESET} ({version}) - {dependencys[dependency]} ready")
+        else:
+            is_complete = True
+            print(f"{Color_line.RED}[NOK] {dependency}:{Color_line.RESET}")
+    if sys.base_prefix == sys.prefix:
+        print(f"\n{Color_line.RED}Your are not in a python virtual environment\nfollow the instruction below for install a python virtual environment:{Color_line.RESET}")
+        venv_instruction()
+        print(f"Dependance installation instruction:\n{Color_line.YELLOW}pip install -r requirements.txt{Color_line.RESET}")
+        print(f" or\n{Color_line.YELLOW}poetry install{Color_line.RESET}")
+
+    return is_complete
+
+if __name__ == "__main__":
+    
+
+    check_dependancy({'pandas': 'Data manipulation',
+                      'numpy':'Numerical computation',
+                      'matplotlib':'Visualization'})
+
+    try:
+        create_data_set()
+    except ModuleNotFoundError:
+        pass
